f63cc102a57bd4d3dc3613aa06bbb7ac6a64b0d3
Alex Fontaine <afontaine@chipkin.com>
2019-08-16 15:21:14 -0700

    Changed namespace to CASBACnetStack

--- BACnetClientExampleCSharp/Program.cs
+++ BACnetClientExampleCSharp/Program.cs
@@ -1,11 +1,11 @@
 ﻿using BACnetServerExample;
-using BACnetStackDLLServerCSharpExample;
+using CASBACnetStack;
 using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 
-namespace BACnetClientExampleCSharp
+namespace CASBACnetStack
 {
     class Program
     {
